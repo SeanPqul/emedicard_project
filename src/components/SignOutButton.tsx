b0692ce83d0ffac41948eba4700edb1c76cdfc35
Ichihara <79244807+SeanPqul@users.noreply.github.com>
2025-07-05 06:46:18 +0800

    feat: implement webhook for Clerk user creation and add user mutation

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useClerk } from '@clerk/clerk-expo'
 import * as Linking from 'expo-linking'
 import { router } from 'expo-router'
@@ -22,4 +23,27 @@ export const SignOutButton = () => {
       <Text>Sign out</Text>
     </TouchableOpacity>
   )
+=======
+import { useClerk } from '@clerk/clerk-expo'
+import { Text, TouchableOpacity } from 'react-native'
+
+export const SignOutButton = () => {
+  // Use `useClerk()` to access the `signOut()` function
+  const { signOut } = useClerk()
+  const handleSignOut = async () => {
+    try {
+      await signOut()
+      //router.replace('/(auth)/sign-in')
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }
+  return (
+    <TouchableOpacity onPress={handleSignOut}>
+      <Text>Sign out</Text>
+    </TouchableOpacity>
+  )
+>>>>>>> Stashed changes
 }
