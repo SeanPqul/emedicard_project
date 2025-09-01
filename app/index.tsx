49fa0a946397efd79fba40ef790da42430233134
Ichihara <79244807+SeanPqul@users.noreply.github.com>
2025-09-01 10:25:15 +0800

    Bug Fixes

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import { Redirect } from 'expo-router';
 
 export default function Index() {
   return <Redirect href="/(auth)/sign-in" />;
 }
-=======
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  return <Redirect href="/(auth)/sign-in" />;
-}
->>>>>>> 05b3e18 (UI Improvement and Bug fixes)
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
@@ -8,15 +7,4 @@ config.transformer.babelTransformerPath = require.resolve('react-native-svg-tran
 config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
 
-=======
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add SVG support
-config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
-
->>>>>>> 05b3e18 (UI Improvement and Bug fixes)
 module.exports = config;
